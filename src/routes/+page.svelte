74763698966a5b50945e110289679a214ffe82e9
Peter Knijff <dev@peterknijff.nl>
2023-01-14 16:39:57 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Navigation from './components/navigation.svelte'
+	import Features from './components/features.svelte'
+	import Pricing from './components/pricing.svelte'
+	import Faq from './components/faq.svelte'
+	import Hero from './components/hero.svelte'
+	import About from './components/about.svelte'
+	import Footer from './components/footer.svelte';
+</script>
+
+<Navigation />
+<Hero />
+<Features />
+<About />
+<Pricing />
+<Faq />
+<Footer />
