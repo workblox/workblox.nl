10d7de54cfb0abc4124138f73a57582e8e2d2142
Peter Knijff <dev@peterknijff.nl>
2023-05-03 21:44:14 +0200

    disable hydration and cardSmall alt text

--- src/lib/components/atoms/cardSmall.svelte
+++ src/lib/components/atoms/cardSmall.svelte
@@ -10,7 +10,7 @@
 	class="{`${clazz}`} flex flex-row w-full items-center lg:mt-0 bg-spring-wood border border-gray-200 rounded-lg shadow py-4 pl-4 pr-12"
 >
 	<div class="flex flex-shrink-0">
-		<img width="44" height="44" src="{image.filename}" loading="lazy">
+		<img width="44" height="44" src="{image.filename}" alt="{image.alt}" loading="lazy">
 	</div>
 	<div class="flex flex-col justify-between p-4 leading-normal">
 		<div>
