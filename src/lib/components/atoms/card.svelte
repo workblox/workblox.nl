a3b8913c027e6f5d93244a5c4497b6236f1f4c8d
Peter Knijff <dev@peterknijff.nl>
2023-05-05 16:52:18 +0200

    update

--- src/lib/components/atoms/card.svelte
+++ src/lib/components/atoms/card.svelte
@@ -1,40 +1,97 @@
 <script lang="ts">
 	export let title;
+	export let index;
 	export let type = 'green';
 	export let items = [];
 	export let image = '';
 
-	let clazz = '';
-	export { clazz as class };
-
-	const typeClassMap = {
-		green: 'bg-[#EBFFDB]',
-		blue: 'bg-[#D5E9F8]',
-		yellow: 'bg-[#FFFBA0]',
-		purple: 'bg-[#C5B6F0]',
-		lightBrown: 'bg-[#FEF4DB]'
-	};
-	import Image from '$lib/components/atoms/image.svelte';
+	const typeClassMap =[
+		['bg-[#EBFFDB]', 'block1'],
+		['bg-[#D5E9F8]', 'block2'],
+		['bg-[#FFFBA0]', 'block3'],
+		['bg-[#C5B6F0]', 'block4'],
+		['bg-[#FEF4DB]', 'block5']
+	];
 </script>
 
 <div
-	class="{`${clazz}`} {typeClassMap[type]} snap-center flex flex-col gap-6 justify-between mt-8 lg:mt-0 w-[85vw] sm:w-[60vw] md:w-[40vw] lg:w-[30vw] xl:w-[28vw] 2xl:w-[20vw] shrink-0 border border-gray-200 rounded-lg shadow p-6 last:mr-4"
+	class="block {typeClassMap[index].join(' ')} snap-center flex gap-6 justify-between mt-8 lg:mt-0 shrink-0 border border-gray-200 rounded-lg shadow p-6"
 >
-	<h4 class="font-med	ium text-2xl text-dark md:text-[28px]">{title}</h4>
-	<ul class="mb-8 space-y-4 text-left text-gray-500 dark:text-gray-400">
-	{#each items as item}
-		<li class="flex items-center space-x-3">
-			<svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-				<path fill="#000" d="M1.6 6.105h4.48V1.6h3.84v4.505h4.48v3.79H9.92V14.4H6.08V9.896H1.6V6.105Z"/>
-			</svg>
-			<span>{item.title}</span>
-		</li>
-	{/each}
-	</ul>
-	<img
-		src={image.filename + '/m/'}
-		alt="{image.alt}"
-		height="{image.filename.split('/')[5].split('x')[0]}"
-		width="{image.filename.split('/')[5].split('x')[1]}"
-	/>
+	<div class="flex block-content">
+		<h4 class="font-medium text-2xl text-dark md:text-[28px] pb-8">{title}</h4>
+		<ul class="mb-8 space-y-4 text-left text-gray-500 dark:text-gray-400">
+			{#each items as item}
+				<li class="flex items-center space-x-3">
+					<svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+						<path fill="#000" d="M1.6 6.105h4.48V1.6h3.84v4.505h4.48v3.79H9.92V14.4H6.08V9.896H1.6V6.105Z"/>
+					</svg>
+					<span>{item.title}</span>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<div class="block-image flex h-full items-center justify-center">
+		<img
+			class="object-cover"
+			src={image.filename + '/m/'}
+			alt="{image.alt}"
+			height="{image.filename.split('/')[5].split('x')[0]}"
+			width="{image.filename.split('/')[5].split('x')[1]}"
+		/>
+	</div>
 </div>
+
+
+<style lang="postcss">
+	.block-image {
+		align-self: center;
+		width: 300px;
+	}
+
+	.block-content {
+		display: flex;
+		flex-flow: column nowrap;
+	}
+
+	.block {
+		display: flex;
+		flex-flow: column nowrap;
+	}
+
+	.block1 {
+		@media (min-width: 960px) {
+			grid-area: 1 / 1 / 5 / 3;
+		}
+
+		.block-image {
+			max-width: 100%;
+		}
+	}
+
+	.block2 {
+		@media (min-width: 960px) {
+			flex-flow: row nowrap;
+			grid-area: 1 / 3 / 3 / 7;
+		}
+	}
+
+	.block3 {
+		@media (min-width: 960px) {
+			flex-flow: row nowrap;
+			grid-area: 3 / 3 / 5 / 7;
+		}
+	}
+
+	.block4 {
+		@media (min-width: 960px) {
+			grid-area: 1 / 1 / 5 / 4;
+		}
+	}
+
+	.block5 {
+		@media (min-width: 960px) {
+			grid-area: 1 / 4 / 5 / 7;
+		}
+	}
+</style>
--- src/lib/components/templates/ListCardSlider.svelte
+++ src/lib/components/templates/ListCardSlider.svelte
@@ -10,23 +10,33 @@
 	use:storyblokEditable={blok}
 >
 	<div class="container">
-		<div class="-mx-4 flex flex-wrap">
+		<div class="flex flex-wrap">
 			<div class="w-full">
 				<div class="mb-12 max-w-[800px] lg:mb-20 px-4">
 					<h2 class="text-3xl font-semibold text-dark sm:text-4xl md:text-[42px]">
 						{blok.title}
 					</h2>
 				</div>
-				<div class="flex gap-4 snap-x pl-4 snap-proximity overflow-x-auto pb-10 hide-scroll-bar">
-					{#each blok.list as { title, list, type, image}, i}
+				<div class="block-top-box-container lg:mb-4 xl:mb-8">
+					{#each blok.list.slice(0, 3) as { title, list, type, image}, i}
 						<Card
 							title={title}
 							items={list}
 							type={type}
 							image={image}
-							class={i > 0 ? 'lg:ml-4' : ''}
-						>
-						</Card>
+							index="{i}"
+						/>
+					{/each}
+				</div>
+				<div class="block-bottom-box-container">
+					{#each blok.list.slice(3, 5) as { title, list, type, image}, i}
+						<Card
+							title={title}
+							items={list}
+							type={type}
+							image={image}
+							index="{i + 3}"
+						/>
 					{/each}
 				</div>
 			</div>
@@ -34,12 +44,27 @@
 	</div>
 </section>
 
-<style>
-	.hide-scroll-bar {
-		-ms-overflow-style: none;
-		scrollbar-width: none;
-	}
-	.hide-scroll-bar::-webkit-scrollbar {
-		display: none;
+<style lang="postcss">
+	.block-top-box-container,
+	.block-bottom-box-container
+	{
+		display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		grid-template-rows: repeat(1, 1fr);
+
+		@media (min-width: 960px) {
+			grid-template-columns: repeat(6, 1fr);
+			grid-template-rows: repeat(4, 1fr);
+		}
+
+		@media (min-width: 960px) {
+			grid-column-gap: 1rem;
+			grid-row-gap: 1rem;
+		}
+
+		@media (min-width: 1140px) {
+			grid-column-gap: 2rem;
+			grid-row-gap: 2rem;
+		}
 	}
 </style>
