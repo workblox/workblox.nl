ca1dc07a0dc02a9c99a578ee8bcfb4e4e0ad38ab
Peter Knijff <dev@peterknijff.nl>
2023-06-09 09:15:15 +0200

    socials & remove analytics / tagm and hotjar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,32 +4,9 @@
 	import Footer from '$lib/components/templates/footer.svelte';
 
 	export let data;
-
-	import { page } from '$app/stores';
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			// eslint-disable-next-line no-undef
-			gtag('config', 'G-KWEP5KJRXB', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
 </script>
 
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-KWEP5KJRXB"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-KWEP5KJRXB');
-	</script>
 </svelte:head>
 
 <Navigation />
