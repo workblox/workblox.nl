cacb88a01661b176ff25e3f52a0a83e79d814ace
Peter Knijff <dev@peterknijff.nl>
2023-07-27 22:08:32 +0200

    add ga4 and hotjar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,41 @@
 	import Footer from '$lib/components/templates/footer.svelte';
 
 	export let data;
+
+
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			// eslint-disable-next-line no-undef
+			gtag('config', 'G-VJ20FJ7CWP', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+
 </script>
 
 <svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VJ20FJ7CWP"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-VJ20FJ7CWP');
+	</script>
+	<script>
+		(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:3530838,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
 </svelte:head>
 
 <Navigation />
