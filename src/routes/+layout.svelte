6f898f896a85392e9452d20354637d34ee121be2
Peter Knijff <dev@peterknijff.nl>
2023-07-29 11:56:32 +0200

    install tagmanager

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,28 +29,15 @@
 	}
 
 	export let data;
-
-	import { page } from '$app/stores';
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			// eslint-disable-next-line no-undef
-			gtag('config', 'G-VJ20FJ7CWP', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
 </script>
 
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VJ20FJ7CWP"></script>
 	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'G-VJ20FJ7CWP');
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WKBHZ8W');
 	</script>
 </svelte:head>
 
